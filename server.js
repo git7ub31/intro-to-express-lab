5bff646b286445cc7aa4f98d9e4896887b40564a
git7ub31 <maryamralmoosa@gmail.com>
2025-02-19 15:31:09 +0300

    lab resubmitted

--- server.js
+++ server.js
@@ -1,76 +1,79 @@
+const express = require('express');
+const app = express();
+const port = 3000;
 
-// 1st question
-const express = require('express')
-const app = express()
+// 1st question:
+app.get('/greetings/:username', (req, res) => {
+    const username = req.params.username;
+    res.send(`Hello there, ${username}`);
+});
 
-app.get('greetings',() =>{
-    alert('a')
-    // res.send(`Hello there, ${req.params.name}`)
-})
+// 2nd question: 
+app.get('/roll/:number', (req, res) => {
+    const rollNum = req.params.number;
 
-//2nd question
-// app.get('/:number',(req,res) => {
-//     alert(req)
-//     // if(req.params.number != Number.IsInteger{
-//     //     res.send("You must specify a number.")
-    // }else( //create a variable and then call it below in send
-    //     math.floor()
-    //     // res.send(`You rolled a ${req.params.number}`)
-    // )
-// })
-// app.get('/role/:number',(req,res) => {
-//     if(isNaN(Number(req.params.number))){
-//         res.send("You must specify a number.")
-//     }else{ //create a variable and then call it below in send
-//        let x = Math.floor(Math.random() * req.params.number)
-//         res.send(`You rolled a ${x}`)
-//     }
-// })
+    if (isNaN(rollNum)) {
+        res.send("You must specify a number.");
+    } else {
+        const randomNum = Math.floor(Math.random() * rollNum);
+        res.send(`You rolled a ${randomNum}`);
+    }
+});
 
-//do the opposite 
-//3rd question > check parse (converts anything into desire)
+// 3rd question: 
+app.get('/collectibles/:index', (req, res) => {
+    const index = parseInt(req.params.index);
 
-// app.get('/collectibles/:number',(req,res) => {
-//     let number = Number(req.params.number)
-//     if(isNaN(number)){
-//         res.send("You must specify a number.")
-//     }else{ //create a variable and then call it below in send
-//         const collectibles = [
-//             { name: 'shiny ball', price: 5.95 },
-//             { name: 'autographed picture of a dog', price: 10 },
-//             { name: 'vintage 1970s yogurt SOLD AS-IS', price: 0.99 }
-//           ];
-//           if (number >= 0 && number < collectibles.length) {
-//             res.send(`So, you want the ${collectibles[number].name}? For ${collectibles[number].price}, it can be yours!`)
-//           } else {
-//             res.send(`This item is not yet in stock. Check back soon!`)
-//           }
-//     }
-// })
+    if (isNaN(index) || index < 0 || index >= collectibles.length) {
+        res.send("This item is not yet in stock. Check back soon!");
+    } else {
+        const collectible = collectibles[index];
+        res.send(`So, you want the ${collectible.name}? For ${collectible.price}, it can be yours!`);
+    }
+});
 
-//4th question
+// 4th question: 
 app.get('/shoes', (req, res) => {
     const shoes = [
-      { name: 'Birkenstocks', price: 50, type: 'sandal' },
-      { name: 'Air Jordans', price: 500, type: 'sneaker' },
-      { name: 'Air Mahomeses', price: 501, type: 'sneaker' },
-      { name: 'Utility Boots', price: 20, type: 'boot' },
-      { name: 'Velcro Sandals', price: 15, type: 'sandal' },
-      { name: 'Jet Boots', price: 1000, type: 'boot' },
-      { name: 'Fifty-Inch Heels', price: 175, type: 'heel' }
-    ]
-    let minNum = Number(req.query.min_price)
-    let maxNum = Number(req.query.max_price)
-    if (req.params.length == null) {
-      res.send(`${shoes}`)
-  } else {
-      if (!isNaN(minNum) && !isNaN(maxNum)) {
-          res.send(`${shoes}`)
-          // if()
-      } else {
-          res.send('${shoes}')
-      }
-    }
-  })
-  
+        { name: 'Birkenstocks', price: 50, type: 'sandal' },
+        { name: 'Air Jordans', price: 500, type: 'sneaker' },
+        { name: 'Air Mahomeses', price: 501, type: 'sneaker' },
+        { name: 'Utility Boots', price: 20, type: 'boot' },
+        { name: 'Velcro Sandals', price: 15, type: 'sandal' },
+        { name: 'Jet Boots', price: 1000, type: 'boot' },
+        { name: 'Fifty-Inch Heels', price: 175, type: 'heel' }
+    ];
 
+    let filteredShoes = shoes;
+
+    // min-price
+    if (req.query['min-price']) {
+        const minPrice = parseFloat(req.query['min-price']);
+        if (!isNaN(minPrice)) {
+            filteredShoes = filteredShoes.filter(shoe => shoe.price >= minPrice);
+        }
+    }
+
+    // max-price
+    if (req.query['max-price']) {
+        const maxPrice = parseFloat(req.query['max-price']);
+        if (!isNaN(maxPrice)) {
+            filteredShoes = filteredShoes.filter(shoe => shoe.price <= maxPrice);
+        }
+    }
+
+    if (req.query['type']) {
+        const type = req.query['type'].toLowerCase();
+        filteredShoes = filteredShoes.filter(shoe => shoe.type.toLowerCase() === type);
+    }
+
+    let response = filteredShoes.map(shoe => {
+        return `Name: ${shoe.name}, Price: ${shoe.price}, Type: ${shoe.type}`;
+    }).join("\n");
+
+    res.send(response);
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
